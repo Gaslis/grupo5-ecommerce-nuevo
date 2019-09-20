ceb699d2b561e5bfb01b6b9c7481bb44d0f18d66
Gas <gaslis@gmail.com>
2019-09-20 08:21:58 -0300

    2.0.1  header transparente
    Prueba del header transparente

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
-<?php include("head.php") ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<?php include("head.php"); ?>
 
   <body>
     <div class="contaniner-fluid">
         <!-- barra de navegacion -->
         <div class="row">
-          <?php include("header.php") ?>
+          <?php include("header.php"); ?>
 
           <!-- Login -->
         <section class="limitesFormu">
@@ -35,7 +37,7 @@
         </section>
       </main>
 
-      <?php include("footer.php") ?>
+      <?php include("footer.php"); ?>
     </div>
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
