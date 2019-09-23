e85a04365547a82cd270299f3c1ee7503826c6de
Gas <gaslis@gmail.com>
2019-09-23 14:00:07 -0300

    2.0.6 cree aside.php

--- index.php
+++ index.php
@@ -76,11 +76,12 @@ $elArray=[
   <?php include("recursos/head.php");?>
   <body>
     <div class="contaniner-fluid">
-        <!-- barra de navegacion -->
         <div class="row">
+					
+<!-- barra de navegacion -->
           <?php include("recursos/header.php");?>
 
-          <!-- Carrusel de imagenes -->
+<!-- Carrusel de imagenes -->
       <section class="carouselMedia posi">
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
           <div class="carousel-inner alturaBanner bordeRedondo">
@@ -105,103 +106,10 @@ $elArray=[
         </div>
       </section>
 
+		<main class="row pl-5 pr-5">
+
 <!-- menu izquierda -->
-      <main class="row pl-5 pr-5">
-        <aside class="col-sm-3 col-md-3 mt-5">
-          <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown">
-              <button type="button" class="btn btn-secondary">NOVEDADES</button>
-              <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          BOLÍGRAFOS</button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                  <a class="dropdown-item" href="#">Plásticos</a>
-                  <a class="dropdown-item" href="#">Metálicos</a>
-                  <a class="dropdown-item" href="#">Ecológicos</a>
-                  <a class="dropdown-item" href="#">Touch</a>
-                  <a class="dropdown-item" href="#">Madera</a>
-                  <a class="dropdown-item" href="#">Cross</a>
-                </div>
-              </div>
-              <button type="button" class="btn btn-secondary">LÁPICES</button>
-              <button type="button" class="btn btn-secondary">ECOLÓGICOS</button>
-              <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">OFICINA Y NEGOCIOS</button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                  <a class="dropdown-item" href="#">Carpetas / Portfolios</a>
-                  <a class="dropdown-item" href="#">Anotadores / Cuadernos</a>
-                  <a class="dropdown-item" href="#">Calendarios</a>
-                  <a class="dropdown-item" href="#">Mouse Pad</a>
-                </div>
-              </div>
-              <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">HOGAR Y TIEMPO LIBRE</button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                  <a class="dropdown-item" href="#">Asado</a>
-                  <a class="dropdown-item" href="#">Herramientas</a>
-                  <a class="dropdown-item" href="#">Juegos</a>
-                  <a class="dropdown-item" href="#">Viajes</a>
-                  <a class="dropdown-item" href="#">Varios</a>
-                </div>
-              </div>
-              <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">TECNOLOGÍA</button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                  <a class="dropdown-item" href="#">Accesorios Celulares</a>
-                  <a class="dropdown-item" href="#">Cargadores - Power Bank</a>
-                  <a class="dropdown-item" href="#">Parlantes - Auriculares</a>
-                </div>
-              </div>
-              <button type="button" class="btn btn-secondary">PEN DRIVES</button>
-              <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">MARROQUINERÍA</button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                  <a class="dropdown-item" href="#">Mochilas</a>
-                  <a class="dropdown-item" href="#">Bolsos</a>
-                  <a class="dropdown-item" href="#">Maletines</a>
-                  <a class="dropdown-item" href="#">Bolsos Térmicos</a>
-                  <a class="dropdown-item" href="#">Valijas</a>
-                  <a class="dropdown-item" href="#">Bolsas</a>
-                  <a class="dropdown-item" href="#">Varios</a>
-                </div>
-                </div>
-                <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    BEBIDAS Y TÉRMICOS
-                </button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                  <a class="dropdown-item" href="#">Botellas Deportivas</a>
-                  <a class="dropdown-item" href="#">Tazas - Vasos</a>
-                  <a class="dropdown-item" href="#">Mates</a>
-                  <a class="dropdown-item" href="#">Sets de Vinos</a>
-                  </div>
-                </div>
-                <button type="button" class="btn btn-secondary">PRODUCTOS DE DISEÑO</button>
-                <button type="button" class="btn btn-secondary">GORROS</button>
-                <button type="button" class="btn btn-secondary">LLAVEROS</button>
-                <button type="button" class="btn btn-secondary">PARAGUAS</button>
-                <button type="button" class="btn btn-secondary">SALUD Y CUIDADO PERSONAL</button>
-                <button type="button" class="btn btn-secondary">RELOJES Y CALCULADORAS</button>
-                <button type="button" class="btn btn-secondary">PVC - SOLDADURA PLÁSTICA</button>
-                <button type="button" class="btn btn-secondary">IMPRESIONES</button>
-                <button type="button" class="btn btn-secondary">TEXTIL</button>
-                <div class="btn-group" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">MARCAS</button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                  <a class="dropdown-item" href="#">Wagner</a>
-                  <a class="dropdown-item" href="#">Cross</a>
-                  <a class="dropdown-item" href="#">Senator</a>
-                  <a class="dropdown-item" href="#">Narda Lepes</a>
-                  <a class="dropdown-item" href="#">LaBaule</a>
-                  <a class="dropdown-item" href="#">Pampero</a>
-                  <a class="dropdown-item" href="#">Slazenger</a>
-                  <a class="dropdown-item" href="#">Swiss Peak</a>
-                  <a class="dropdown-item" href="#">Moleskine</a>
-                  <a class="dropdown-item" href="#">Pierre cardin</a>
-                  <a class="dropdown-item" href="#">Fast Travel</a>
-                </div>
-              </div>
-            </div>
-          </aside>
+			<?php include("recursos/aside.php");?>
 
 <!-- Productos a la venta -->
           <section class="col-12 col-sm-12 col-md-9">
